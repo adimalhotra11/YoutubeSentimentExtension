25d86586a1538478aafadd843efdd3e6d8ac2b3f
Aditya Malhotra <adityamalhotramcs@gmail.com>
2022-05-13 14:58:04 +0530

    Added Function to Extract data using Youtube API.

---
+++ CommentExtractor.py
@@ -0,0 +1,29 @@
+def load_comments(match):
+    for item in match["items"]:
+        comment = item["snippet"]["topLevelComment"]
+        author = comment["snippet"]["authorDisplayName"]
+        text = comment["snippet"]["textDisplay"]
+        print("Comment by user {}: {}".format(author, text))
+        comments.append(text)
+
+
+def get_comment_threads(youtube, video_id, limit):
+    results = youtube.commentThreads().list(
+        part="snippet",
+        maxResults=limit,
+        videoId=video_id,
+        textFormat="plainText"
+    ).execute()
+    return results
+
+def get_comment_thread(youtube, video_id, mytoken, limit):
+    results = youtube.commentThreads().list(
+        part="snippet",
+        maxResults=limit,
+        videoId=video_id,
+        textFormat="plainText",
+        pageToken=mytoken
+    ).execute()
+    return results
+
+comments = []
--- Extraction.py
+++ Extraction.py
@@ -1,39 +1,12 @@
-import Hidden
+import Hidden #import API key and service details
+import CommentExtractor
 from apiclient.discovery import build
 
 youtube = build(Hidden.YOUTUBE_API_SERVICE_NAME,
               Hidden.YOUTUBE_API_VERSION,
               developerKey=Hidden.api_key)
 
-ucom = []
-
-def load_comments(match):
-    for item in match["items"]:
-        comment = item["snippet"]["topLevelComment"]
-        author = comment["snippet"]["authorDisplayName"]
-        text = comment["snippet"]["textDisplay"]
-        print("Comment by user {}: {}".format(author, text))
-        ucom.append(text)
-
-
-def get_comment_threads(youtube, video_id, limit):
-    results = youtube.commentThreads().list(
-        part="snippet",
-        maxResults=limit,
-        videoId=video_id,
-        textFormat="plainText"
-    ).execute()
-    return results
-
-def get_comment_thread(youtube, video_id, mytoken, limit):
-    results = youtube.commentThreads().list(
-        part="snippet",
-        maxResults=limit,
-        videoId=video_id,
-        textFormat="plainText",
-        pageToken=mytoken
-    ).execute()
-    return results
+comments = []  #this will store all the comments we will be extracting using the API
 
 limit1 = 100
 limit = int(input("Enter number of comments: "))
@@ -49,8 +22,8 @@ else:
   count=0
   limit1=limit
 
-match = get_comment_threads(youtube, video_id, limit1)
+match = CommentExtractor.get_comment_threads(youtube, video_id, limit1)
 next_page_token = match["nextPageToken"]
-load_comments(match) 
+CommentExtractor.load_comments(match) 
 
-print(len(ucom))
+print(len(comments))
